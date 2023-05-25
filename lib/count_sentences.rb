1fe0b442acc4b1614a118d072e84dd7b76199dfd
lyonbett <lyon.bett@student.moringaschool.com>
2023-05-25 11:47:06 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.nil? || self.empty? ? 0 : self.split(/[.?!]+/).count 
   end
 end
